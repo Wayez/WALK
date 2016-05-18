3e99491946ab128aea9c0b70fe980cafeb7c2ba1
kagers <ka.gershfeld@gmail.com>
2016-05-18 09:37:35 -0400

    INSERT INTERESTING COMMIT MESSAGE HERE

--- static/example.js
+++ static/example.js
@@ -1,35 +1,10 @@
-/*(var doubleElimination = {
-    "teams": [
-	["Team 1", "Team 2"],
-	["Team 3", "Team 4"]
-    ],
-    "results": [            // List of brackets (three since this is double elimination)
-	[                     // Winner bracket
-	    [[1, 2], [3, 4]],   // First round and results
-	    [[5, 6]]            // Second round
-	],
-	[                     // Loser bracket
-	    [[7, 8]],           // First round
-	    [[9, 10]]           // Second round
-	],
-	[                     // Final "bracket"
-	    [                   // First round
-		[11, 12],         // Match to determine 1st and 2nd
-		[13, 14]          // Match to determine 3rd and 4th
-	    ],
-	    [                   // Second round
-		[15, 16]          // LB winner won first round (11-12) so need a final decisive round
-	    ]
-	]
-    ]
-};*/
 
 var doubleEliminationData = {
-    teams : [
+    "teams" : [
       ["Team 1", "Team 2"],
       ["Team 3", "Team 4"]
     ],
-    results : [[      /* WINNER BRACKET */
+    "results" : [[      /* WINNER BRACKET */
       [[1,2], [3,4]], /* first and second matches of the first round */
       [[5,6]]         /* second round */
     ], [              /* LOSER BRACKET */
@@ -39,9 +14,10 @@ var doubleEliminationData = {
       [[1,12], [13,14]],
       [[15,16]]       /* LB winner won first round so need a rematch */
     ]]
-};
+}
  
 $(function() {
-    $('div#doubleElimination. demo').bracket({
-      init: doubleEliminationData})
-});
+    $('div#doubleElimination.demo').bracket({
+	init: doubleEliminationData})
+})
+
