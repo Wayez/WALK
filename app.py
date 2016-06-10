06f811a684129d2f098cd0f7bd43b0b216cd6a46
Wayez Chowdhury <c.wayez3793@gmail.com>
2016-06-09 23:22:09 -0400

    please dont have any merge conflicts :worried:

--- app.py
+++ app.py
@@ -78,7 +78,10 @@ def home_user():
     if request.method == 'POST':
         #print request.form
         if request.form.has_key('new'):
-            tid = mongoutils.getTeamId(request.form['old'])
+            #print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+            #print request.form
+            #print "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+            tid = mongoutils.getTeamId(request.form['joinTeam'])
             return redirect("/team/" + str(tid))
         if request.form.has_key('old'):
             tid = mongoutils.getTeamId(request.form['old'])
--- mongoutils.py
+++ mongoutils.py
@@ -276,7 +276,7 @@ Returns:
     id of tournament
 '''
 def getTournId(name):
-    result = tournsc.find_one({'name':name},{'_id':1})
+    result = tournsc.findOne({'name':name},{'_id':1})
     return result['_id']
 
 '''
@@ -377,8 +377,12 @@ def getTeam(ida):
 #print getTeam(1)
 
 def getTeamId(name):
-    result = teamsc.find_one({'name':name},{'_id':1})
+    print name
+    result = teamsc.find_one({'name':name})
+    print result
     return result['_id']
+
+print getTeamId('Walking')
 
 def getCoachTeams(coach):
     result = teamsc.find({'coach':coach},{'name':1})
